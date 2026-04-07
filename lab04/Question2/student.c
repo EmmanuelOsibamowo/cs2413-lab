5f35b4aefa92a097b9d3438c53ef83320928587e
EmmanuelOsibamowo <eosibamowo@gmail.com>
2026-04-07 18:25:14 -0500

    Update student.c
    Solved question 2 of Lab04

--- lab04/Question2/student.c
+++ lab04/Question2/student.c
@@ -50,6 +50,23 @@ struct TreeNode {
 
 
 
-bool isSymmetric(struct TreeNode* root) {
+bool isMirror(struct TreeNode* t1, struct TreeNode* t2) {
   // TODO: implement
+        if (t1 == NULL && t2 == NULL) {
+        return true;
+    }
+    if (t1 == NULL || t2 == NULL) {
+        return false;
+    }
+
+    return (t1->val == t2->val) &&
+           isMirror(t1->left, t2->right) &&
+           isMirror(t1->right, t2->left);
+}
+
+bool isSymmetric(struct TreeNode* root) {
+    if (root == NULL) {
+        return true;
+    }
+    return isMirror(root->left, root->right);
 }
