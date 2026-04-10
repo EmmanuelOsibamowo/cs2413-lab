d5275cae60beef89ad0bf781f996f57efe78614a
EmmanuelOsibamowo <eosibamowo@gmail.com>
2026-04-09 23:57:14 -0500

    Q3 solved for Lab06
    Q3 solved for Lab06

--- lab06/Question3/student.c
+++ lab06/Question3/student.c
@@ -1,13 +1,33 @@
 #include "graph.h"
 
-// Description
-
-// Check whether the graph contains a triangle. A triangle is a set of three nodes that are all connected to each other. 
-// The graph is stored as an adjacency matrix. 
-// Graph* g is a pointer to a Graph struct, which contains a 2D array adj[MAX_NODES][MAX_NODES]. 
-// The value at adj[u][v] is 1 if there is an edge between nodes u and v, and 0 otherwise.
-
+/**
+ * Checks if the graph contains at least one triangle (3-clique).
+ * Returns 1 if a triangle exists, 0 otherwise.
+ */
 int has_triangle(Graph* g) {
-    // TODO: implement
-    // return -1;
+    int n = g->num_nodes;
+
+    // We need at least 3 nodes to form a triangle
+    if (n < 3) {
+        return 0;
+    }
+
+    // Iterate through all possible triplets of nodes (i, j, k)
+    // We use i < j < k to ensure we pick distinct nodes and don't duplicate checks
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            // Only proceed if there is an edge between i and j
+            if (g->adj[i][j] == 1) {
+                for (int k = j + 1; k < n; k++) {
+                    // Check if node k completes the triangle with i and j
+                    if (g->adj[j][k] == 1 && g->adj[k][i] == 1) {
+                        return 1; // Triangle found!
+                    }
+                }
+            }
+        }
+    }
+
+    // If we've checked all triplets and found nothing
+    return 0;
 }
