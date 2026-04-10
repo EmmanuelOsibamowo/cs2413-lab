ef071fec0aef254b5cff4c2941701227231e60ce
EmmanuelOsibamowo <eosibamowo@gmail.com>
2026-04-09 23:16:27 -0500

    Solved Q2 of Lab06
    Solved Q2 of Lab06

--- lab06/Question2/student.c
+++ lab06/Question2/student.c
@@ -1,4 +1,5 @@
 #include "graph.h"
+#include <stdbool.h>
 // Description
 
 // Write a function that counts how many nodes are isolated.
@@ -11,11 +12,29 @@
 
 
 int count_isolated(Graph* g) {
-    // TODO: implement
-    // return -1;
-   
-}
+    int isolated_count = 0;
 
+    // Iterate through every node in the graph
+    for (int i = 0; i < g->num_nodes; i++) {
+        bool has_edge = false;
+
+        // Check the entire row for node 'i'
+        for (int j = 0; j < g->num_nodes; j++) {
+            // If we find a 1, this node is connected to something
+            if (g->adj[i][j] == 1) {
+                has_edge = true;
+                break; // No need to check the rest of the row
+            }
+        }
+
+        // If we finished the inner loop and found no 1s, the node is isolated
+        if (!has_edge) {
+            isolated_count++;
+        }
+    }
+
+    return isolated_count;
+}
 
 
 
